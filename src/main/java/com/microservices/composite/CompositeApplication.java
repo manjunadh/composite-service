291fa193df708d1b05a50157c0f0093c98d1c231
Manjunadh Veeravalli <P7111009@NESSINBLR.COM>
2017-03-15 19:08:28 +0530

    comosite service

---
+++ src/main/java/com/microservices/composite/CompositeApplication.java
@@ -0,0 +1,45 @@
+package com.microservices.composite;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
+
+import zipkin.Span;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableAsync
+public class CompositeApplication {
+	private static final Log log = LogFactory.getLog(CompositeApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	@ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "false")
+	public ZipkinSpanReporter spanCollector() {
+		return new ZipkinSpanReporter() {
+
+			@Override
+			public void report(Span span) {
+
+				log.info(String.format("Reporting span [%s]", span));
+			}
+
+		};
+	}
+
+}
